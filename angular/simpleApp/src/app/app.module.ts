58ef630f7fe7b9600d98ff92b11a9511f51db4b7
shahbazaamir <shahbaz07dbit@gmail.com>
2018-06-09 00:53:11 +0530

    tweek

--- angular/simpleApp/src/app/app.module.ts
+++ angular/simpleApp/src/app/app.module.ts
@@ -3,55 +3,24 @@ import { NgModule } from '@angular/core';
 import {Routes,RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { QuestionComponent } from './question/question.component';
 import { HomeComponent } from './home/home.component';
-import { SecuritiesComponent } from './securities/securities.component';
-import { BuyComponent } from './securities/buy/buy.component';
-import { BuypriceComponent } from './securities/buyprice/buyprice.component';
-import { SellpriceComponent } from './securities/sellprice/sellprice.component';
-import { StocksComponent } from './securities/stocks/stocks.component';
-import { ChangeComponent } from './securities/change/change.component';
-import { BrokerageComponent } from './securities/brokerage/brokerage.component';
-import { ProfitComponent } from './securities/profit/profit.component';
-import { NetprofitComponent } from './securities/netprofit/netprofit.component';
-import { RecidentialstatusComponent } from './securities/recidentialstatus/recidentialstatus.component';
-import { ScripComponent } from './securities/scrip/scrip.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { HttpModule } from '@angular/http';
-import { QuestionService } from './question/question.service';
 
 const appRoutes:Routes =[
-{path:'home',component: HomeComponent},
-{path:'securities',component: SecuritiesComponent},
-{path:'projects',component: ProjectsComponent},
-{path:'question',component: QuestionComponent}
-{path:'subject',component: QuestionComponent},
+{path:'home',component: HomeComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionComponent,
-    HomeComponent,
-    SecuritiesComponent,
-    BuyComponent,
-    BuypriceComponent,
-    SellpriceComponent,
-    StocksComponent,
-    ChangeComponent,
-    BrokerageComponent,
-    ProfitComponent,
-    NetprofitComponent,
-    RecidentialstatusComponent,
-    ScripComponent,
-    ProjectsComponent
+   
+    HomeComponent
   ],
   imports: [
     BrowserModule,
 	HttpModule,
 	RouterModule.forRoot(appRoutes)
   ],
-  providers: [QuestionService],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
