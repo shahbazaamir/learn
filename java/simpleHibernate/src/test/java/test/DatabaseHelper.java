968872e246b99b9062eb506a1d52d96b8d87758a
shahbazaamir <shahbaz07dbit@gmail.com>
2018-07-29 15:49:55 +0530

    added get

--- java/simpleHibernate/src/test/java/test/DatabaseHelper.java
+++ java/simpleHibernate/src/test/java/test/DatabaseHelper.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -8,15 +9,16 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 //import javax.xml.bind.JAXBException;
+import org.junit.Test;
 
 import junit.framework.TestCase;
 import test.Order;
 
-public class DatabaseHelper extends TestCase {
+public class DatabaseHelper {
 	private SessionFactory sessionFactory;
 
 	private static StandardServiceRegistry registry;
-	@Override
+	
 	protected void setUp() throws Exception {
 		// A SessionFactory is set up once for an application!
 		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -32,7 +34,6 @@ public class DatabaseHelper extends TestCase {
 		}
 	}
 
-	@Override
 	protected void tearDown() throws Exception {
 		if ( sessionFactory != null ) {
 			sessionFactory.close();
@@ -44,12 +45,27 @@ public class DatabaseHelper extends TestCase {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		Order order =new Order();
-		order.setId("12");
+		order.setId("123");
 		order.setScriptName("TEST");
 		order.setStockPrice(new BigDecimal(100));
 		order.setStocks(new BigDecimal(1));
 		session.save( order );
 		session.getTransaction().commit();
 		session.close();
+	}
+	
+	
+	@Test
+	public void getAllOrder() throws Exception {
+		setUp();
+		List result =null;
+		try{
+			Session s = sessionFactory.openSession();
+			 result = s.createQuery( "from Order" ).list();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println( result);
+		tearDown();
 	}
 }
