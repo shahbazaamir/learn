e07800bc31c1f705b3b1fc1a1bc8f5d7bf009956
shahbaz aamir <shahbaz07dbit@gmail.com>
2018-08-11 00:19:02 +0530

    updated
    updated

--- projects/order/orderService/src/main/java/org/order/service/OrderDetails.java
+++ projects/order/orderService/src/main/java/org/order/service/OrderDetails.java
@@ -1,5 +1,21 @@
 package org.order.service;
 // pull success , push success
-public class OrderDetails {
 
+import java.io.Serializable;
+import java.util.List;
+
+public class OrderDetails<T> implements Serializable {
+
+	private List<T> orderList;
+
+	public List<T> getOrderList() {
+		return orderList;
+	}
+
+	public void setOrderList(List<T> orderList) {
+		this.orderList = orderList;
+	}
+	
+	
+	
 }
