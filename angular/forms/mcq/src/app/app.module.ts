2089f02963d0bfcef55c14adcb1b2d42cf3f1046
shahbaz aamir <shahbaz07dbit@gmail.com>
2019-02-19 22:43:36 +0530

    forms added

---
+++ angular/forms/mcq/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {Routes,RouterModule} from '@angular/router';
+import { CustDetailComponent } from './cust-detail/cust-detail.component';
+import { MCQComponent } from './mcq/mcq.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+const appRoutes:Routes =[
+{path:'cust-detail',component: CustDetailComponent},
+{path:'mcq',component: MCQComponent},
+{path:'sign-up',component: SignUpComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustDetailComponent,
+    MCQComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+	FormsModule,
+	ReactiveFormsModule,
+	RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/forms/mcq/src/app/mcq/mcq.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mcq',
+  templateUrl: './mcq.component.html',
+  styleUrls: ['./mcq.component.css']
+})
+export class MCQComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
