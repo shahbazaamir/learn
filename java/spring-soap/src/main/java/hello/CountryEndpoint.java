cb56f8a8191398185d8fa1131af8f5a3f6675cf1
shahbaz aamir <shahbaz07dbit@gmail.com>
2019-07-06 18:42:30 +0530

    spring soap

---
+++ java/spring-soap/src/main/java/hello/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import localhost._8093.guides.gs_producing_web_service.GetCountryRequest;
+import localhost._8093.guides.gs_producing_web_service.GetCountryResponse;
+ 
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://localhost:8093/guides/gs-producing-web-service";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
