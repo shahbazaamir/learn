35faace28ab9ad2244f409c8fe6aae32a126a408
shahbaz aamir <shahbaz07dbit@gmail.com>
2019-12-07 21:21:45 +0530

    db connected

---
+++ springboot/pdfGeneration/pdf/src/main/java/com/progvocab/pdf/controller/PdfController.java
@@ -0,0 +1,33 @@
+package com.progvocab.pdf.controller;
+ 
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.progvocab.pdf.service.PdfService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class PdfController {
+	
+	@Autowired
+	private PdfService pdfService;
+	
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping("/question")
+    public String index() {
+        return "[{\"id\":\"1\",\"questionDesc\":\"What is java?\"}]";
+    }
+	
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping("/pdf")
+    public String generatePdf() {
+		pdfService.generatePdf();
+		return "success";
+    }
+    
+}
---
+++ springboot/pdfGeneration/pdf/src/main/java/com/progvocab/pdf/repository/Mytable1Repository.java
@@ -0,0 +1,9 @@
+package com.progvocab.pdf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.progvocab.pdf.entity.Mytable1;
+
+public interface Mytable1Repository extends JpaRepository<Mytable1, Integer>{
+
+}
