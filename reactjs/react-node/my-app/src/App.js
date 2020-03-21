27adeb69fd650993524fda7bf651265e4e308a50
shahbaz aamir <shahbaz07dbit@gmail.com>
2020-03-22 01:24:46 +0530

    react

--- reactjs/react-node/my-app/src/App.js
+++ reactjs/react-node/my-app/src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Top from './header/Top.js';
+import TestHook from './header/TestHook.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Top></Top>
-      </header>
+     
+         
+         
+        <TestHook></TestHook>
+       
+
     </div>
   );
 }
---
+++ reactjs/react-node/my-app/src/header/TestHook.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+ class TestHook extends React.Component {
+    constructor() {
+      super();
+      this.state = {color: "red"};
+    }
+    render() {
+      return <div>
+        <h2>Test Hook</h2>
+        
+        
+        </div>;
+    }
+  }
+
+  export default TestHook;
