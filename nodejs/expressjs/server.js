cf868cc09f7753c72da2e858aae328207583c3c9
shahbaz aamir <shahbaz07dbit@gmail.com>
2020-03-21 14:31:44 +0530

    express

--- nodejs/expressjs/server.js
+++ nodejs/expressjs/server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 
+var users = require('./service/users.js');
+
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+//app.get('/', function (req, res) {
+  // res.send('Hello World');
+//})
 
-var server = app.listen(8081, function () {
+app.use('/users', users);
+
+var server = app.listen(8082, function () {
    var host = server.address().address;
    var port = server.address().port;
 
