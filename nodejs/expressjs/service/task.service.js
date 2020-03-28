49ca26ea3ac22978c5005e4c413be0020d4a9949
shahbaz aamir <shahbaz07dbit@gmail.com>
2020-03-28 12:59:54 +0530

    expressjs

---
+++ nodejs/expressjs/service/task.service.js
@@ -0,0 +1,12 @@
+import connection from 'db.js';
+
+
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + result);
+  });
+});
