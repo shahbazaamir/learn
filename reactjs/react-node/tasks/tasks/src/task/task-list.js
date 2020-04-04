9062133435557764c66824171098306cdf990ec3
shahbaz aamir <shahbaz07dbit@gmail.com>
2020-04-05 03:11:54 +0530

    tasks

---
+++ reactjs/react-node/tasks/tasks/src/task/task-list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Task from './task.js';
+
+
+
+function TaskList() {
+  return (
+    <div  >
+       <Task></Task>
+    </div>
+  );
+}
+
+export default TaskList;
